219c4c3016b05a925aa133b996a11a192aef73d0
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2022-11-29 23:03:07 +0530

    cmnt

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import { useGlobalContext } from './context'
 const CartItem = ({ id, img, title, price, amount }) => {
-  const { remove, increase, decrease, toggleAmount } = useGlobalContext()
+  const { remove, toggleAmount } = useGlobalContext()
   return (
     <article className='cart-item'>
       <img src={img} alt={title} />
       <div>
         <h4>{title}</h4>
         <h4 className='item-price'>${price}</h4>
-        {/* remove button */}
         <button className='remove-btn' onClick={() => remove(id)}>
           remove
         </button>
       </div>
       <div>
-        {/* increase amount */}
         <button className='amount-btn' onClick={() => toggleAmount(id, 'inc')}>
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
             <path d='M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z' />
