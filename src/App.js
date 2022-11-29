ddf8b77a2bfae299f72dea850ed221780833a14e
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2022-11-29 23:02:07 +0530

    cmnt

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import { useGlobalContext } from './context'
-
-// components
 import Navbar from './Navbar'
 import CartContainer from './CartContainer'
-// items
 
 function App() {
   const { loading } = useGlobalContext()
