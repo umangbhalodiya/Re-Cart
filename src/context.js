efb53dc60d15ad54c84ca24342551260b4527291
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2022-11-29 23:04:20 +0530

    cmnt

--- src/context.js
+++ src/context.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useReducer, useEffect } from 'react'
+import React, { useContext, useReducer, useEffect } from 'react'
 import cartItems from './data'
 import reducer from './reducer'
 const url = 'https://course-api.com/react-useReducer-cart-project'
